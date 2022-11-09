ed7d3b4f2bb3c3170b157f75cdebcf9def354a6b
amon-cofie <ddd.amon123@gmail.com>
2022-11-09 08:27:51 +0000

    Implement add book and remove book functions

--- src/components/FormInput.js
+++ src/components/FormInput.js
@@ -1,11 +1,31 @@
 import React from 'react';
+import { v4 as uuidv4 } from 'uuid';
+import { useDispatch } from 'react-redux';
+import { addBook } from '../redux/books/books';
 
-const FormInput = () => (
-  <div>
-    <input type="text" placeholder="Title" />
-    <input type="text" placeholder="Author" />
-    <button type="submit">ADD SCORE</button>
-  </div>
-);
+const FormInput = () => {
+  const dispatch = useDispatch();
+  return (
+    <div>
+      <input type="text" placeholder="Title" id="title" />
+      <input type="text" placeholder="Author" id="author" />
+      <button
+        type="submit"
+        onClick={(e) => {
+          e.preventDefault();
+          dispatch(
+            addBook({
+              id: uuidv4(),
+              title: document.getElementById('title').value,
+              author: document.getElementById('author').value,
+            }),
+          );
+        }}
+      >
+        ADD SCORE
+      </button>
+    </div>
+  );
+};
 
 export default FormInput;
